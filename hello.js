b1a5362a144da27125e488a61e1261efd03c8948
Pancham DM_055 <pancham@admins-MacBook-Pro.local>
2026-08-19 13:03:55 +0530

    f1

--- hello.js
+++ hello.js
@@ -2,6 +2,9 @@ exports.handler = async (event) => {
   const mySecret = process.env.MY_SECRET;
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
     body: JSON.stringify({ message: "NOOOO!", secret: mySecret }),
   };
 };
--- random.js
+++ random.js
@@ -3,6 +3,9 @@ exports.handler = async (event) => {
 
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
     body: JSON.stringify({ id: uuidv4()+" test of control" }),
   };
 };
